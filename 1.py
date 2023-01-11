d98af36b8bb8ab70826f694ea1cea0e4d5072a4f
hongmao <778716966@qq.com>
2023-01-12 06:44:13 +0800

    'cell' finish 	deleted:    Arbiter/11.png 	modified:   README.md

--- 1.py
+++ 1.py
@@ -1,7 +1,19 @@
 import pyautogui
 import time
 
-# pyautogui.moveTo(1798, 848)
+# numbering start from one
+NX, NY = 16, 30
+N = NX * NY
+UPPER, LOWER, LEFT, RIGHT = 5, 4, 5, 4
+CELLX, CELLY = 24, 24
+
+
+def GetId(x, y):
+    return (x - 1) * NY + y
+
+
+def GetXY(id):
+    return (id - 1) // NY + 1, (id - 1) % NY + 1
 
 
 def main():
@@ -9,13 +21,33 @@ def main():
     difficulty = 'Expert'
     difficulty_fig = './Arbiter/difficulty/' + difficulty + '.png'
     location = pyautogui.locateOnScreen(difficulty_fig)
+    print('{:.^30}'.format('Finding'))
+    while location == None:
+        location = pyautogui.locateOnScreen(difficulty_fig)
+    print('{:.^30}'.format('\'location\' ready'))
     print(location)
-    init = pyautogui.locateAllOnScreen('./Arbiter/11.png')
-    tot = 0
-    for i in init:
-        print(i)
-        tot += 1
-    print(tot)
+    global left, top
+    left, top = location[0], location[1]
+    cell = [[0, 0] for i in range(N + 1)]
+    # x-axis direction:top to bottom
+    # y-axis direction:left to right
+    # cell stores the coordinates of the upper left corner of all cells
+    for i in range(1, N + 1):
+        x, y = GetXY(i)
+        print(i, x, y)
+        if (x == 1):
+            cell[i][1] = top + UPPER
+            if(y == 1):
+                cell[i][0] = left + LEFT
+            else:
+                cell[i][0] = cell[i - 1][0] + CELLY
+        else:
+            cell[i][1] = cell[i - NY][1] + CELLX
+            if(y == 1):
+                cell[i][0] = left + LEFT
+            else:
+                cell[i][0] = cell[i - 1][0] + CELLY
+        pyautogui.click(cell[i][0], cell[i][1], button='right')
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 根据MinesweeperArbiter游戏界面,使用python编写,基于pyautogui图像识别
 
 电脑分辨率2560*1440,游戏界面大小为软件默认大小
+
+暂仅支持30*16(Expert)大小
